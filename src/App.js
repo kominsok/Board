2d083fe8b4b4de34dfa758847f0d4890e5dd562e
kominsok <kominsok@naver.com>
2022-12-20 21:52:19 +0900

    적용

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from'./components/Navbar';
 import Home from'./pages/Home'; // 추가 된 내용
-import Station from'./pages/Station';
+import Station from'./pages/station/Station';
 import Voc from'./pages/Voc';
 
 function App() {
